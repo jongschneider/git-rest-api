f1513e45ba290c52f65b86cf987947239cd29810
Jonathan Schneider <jongschneider@gmail.com>
2017-05-26 13:43:06 -0400

    fb workings... svaed facebook.model.js

---
+++ src/modules/users/facebook.model.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from 'mongoose';
+import validator from 'validator';
+import jwt from 'jsonwebtoken';
+import uniqueValidator from 'mongoose-unique-validator';
+
+import constants from '../../config/constants';
+
+const FacebookSchema = new Schema(
+	{
+		id: {
+			type: String,
+			trim: true
+		},
+		token: {
+			type: String,
+			trim: true
+		},
+		email: {
+			type: String,
+			trim: true
+		},
+		name: {
+			type: String,
+			trim: true
+		},
+		firstName: {
+			type: String,
+			trim: true
+		},
+		lastName: {
+			type: String,
+			trim: true
+		}
+	},
+	{ timestamps: true }
+);
+
+FacebookSchema.methods = {
+	createToken() {
+		return jwt.sign(
+			{
+				_id: this._id
+			},
+			constants.JWT_SECRET
+		);
+	},
+	toJSON() {
+		return {
+			id: this._id,
+			name: this.name,
+			token: `JWT ${this.createToken()}`
+		};
+	}
+};
+
+export default mongoose.model('FBUser', FacebookSchema);
