d4dce93b3776f23280433a95066037c86cd6b004
Jonathan Schneider <jongschneider@gmail.com>
2017-05-27 12:22:35 -0400

    removed some comments and such.

--- src/modules/users/user.controllers.js
+++ src/modules/users/user.controllers.js
@@ -14,34 +14,3 @@ export function login(req, res, next) {
 
 	return next();
 }
-
-// export async function fbSignup(err, user) {
-// 		if (err) {
-// 			return cb(err, false);
-// 		}
-// 		if (user) {
-// 			return cb(null, user);
-// 		} else {
-// 			const newUser = new FBUser();
-// 			newUser.id = profile.id;
-// 			newUser.token = accessToken;
-// 			newUser.email = profile.emails[0].value;
-// 			newUser.name = profile.name.givenName + ' ' + profile.name.familyName;
-// 			newUser.firstName = profile.name.givenName;
-// 			newUser.lastName = profile.name.familyName;
-//
-// 			newUser.save(function(err) {
-// 				if (err) {
-// 					throw err;
-// 				}
-// 				return cb(null, newUser);
-// 			});
-// 		}
-// 	});
-// });
-//
-// console.log('Your accessToken is :' + accessToken);
-// console.log('Your refreshToken is :' + refreshToken);
-// console.log('Your profile is :');
-// console.log(profile);
-// return cb(null, profile);)
